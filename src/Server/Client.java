cd3eece7e6affb0b63445a0877221cb8e7cbe9c4
rbradt <rbradt1@saddleback.edu>
2020-03-11 16:01:33 -0700

    added client functions

--- src/Server/Client.java
+++ src/Server/Client.java
@@ -1,18 +1,60 @@
-package ServerPackage;
+package Server;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+import Messages.Packet;
 
 public class Client implements Runnable {
     private int index;
+    private String name;
+    private boolean connection;
+    private Socket socket;
+    private DataInputStream in;
+    private DataOutputStream out;
 
-    public Client(int index, ){}
+    public Client(int index, String name, Socket socket) {
+        this.index = index;
+        this.name = name;
+        this.socket = socket;
+        connection = true;
+    }
 
     public int getIndex() {
         return index;
     }
 
-    public void sendPacket(Packet p){}
+    public void terminateConnection() {
+        connection = false;
+    }
+
+    public void sendPacket(Packet p) {
+        try {
+            out = new DataOutputStream(socket.getOutputStream());
+
+            // send packet to client
+        }
+        catch(IOException e) {
+            e.printStackTrace();
+        }
+    }
 
     @Override
     public void run() {
+        try {
+            in = new DataInputStream(socket.getInputStream());
+
+            while(connection) {
+                // serve client until client disconnects
+            }
+        }
+        catch(IOException e) {
+            e.printStackTrace();
+        }
+        finally {
+            // delete this object when it disconnects
+        }
 
     }
 }
