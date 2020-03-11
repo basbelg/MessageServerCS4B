84029a86e421f4454b6933f1260fb2c0249a10ba
rbradt <rbradt1@saddleback.edu>
2020-03-11 16:02:31 -0700

    added server functions

--- src/Server/Server.java
+++ src/Server/Server.java
@@ -1,31 +1,44 @@
-package ServerPackage;
+package Server;
 
 import java.io.IOException;
 import java.io.Serializable;
 import java.net.ServerSocket;
-import java.util.ArrayList;
-import java.util.HashMap;
+import java.net.Socket;
+import java.util.*;
 import java.util.concurrent.BlockingQueue;
 
 public class Server implements Runnable {
     public static BlockingQueue<Serializable> requests;
 
     private int port;
+    private boolean shutdown;
+    private Socket socket;
     private ServerSocket serverSocket;
-
-    private ArrayList<Client> ConnectedClients;
-    private HashMap<String, Integer> SubscriberList;
+    private List<Client> connectedClients;
+    private HashMap<String, List<Integer>> subscriberList;
 
     public Server() {
         port = 8000;
-        ConnectedClients = new ArrayList<>();
-        SubscriberList = new HashMap<>();
+        shutdown = false;
+    }
+
+    public void terminateServer() {
+        shutdown = true;
     }
 
     @Override
     public void run() {
         try {
             serverSocket = new ServerSocket(port);
+            connectedClients = new ArrayList<>();
+            subscriberList = new HashMap<>();
+
+            while(!shutdown) {
+                // wait on client connection
+                socket = serverSocket.accept();
+
+                // manage client connection
+            }
         }
         catch(IOException e) {
             e.printStackTrace();
