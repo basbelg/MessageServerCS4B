c25ede539fbd74be29563716808e15e05041d498
basbelg <basbeez@live.com>
2020-03-21 01:58:25 -0700

    RegistrationMsg - Deleted the default constructor, since we never want to create an 'empty' user

--- src/Messages/RegistrationMsg.java
+++ src/Messages/RegistrationMsg.java
@@ -6,10 +6,6 @@ public class RegistrationMsg {
     private String username;
     private List<String> subscribedChannels;
 
-    public RegistrationMsg() {
-        this("<UNKOWN>", null);
-    }
-
     public RegistrationMsg(String username, List<String> subscribedChannels) {
         this.username = username;
         this.subscribedChannels = subscribedChannels;
