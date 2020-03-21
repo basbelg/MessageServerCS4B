ee0f4ac8d33dcde931c3aa5fa18a3fba76493eca
basbelg <basbeez@live.com>
2020-03-20 19:29:22 -0700

    Added a default constructor and an alternate constructor to the RegistrationMsg class

--- src/Messages/RegistrationMsg.java
+++ src/Messages/RegistrationMsg.java
@@ -5,4 +5,13 @@ import java.util.List;
 public class RegistrationMsg {
     private String username;
     private List<String> subscribedChannels;
+
+    public RegistrationMsg() {
+        this("<UNKOWN>", null);
+    }
+
+    public RegistrationMsg(String username, List<String> subscribedChannels) {
+        this.username = username;
+        this.subscribedChannels = subscribedChannels;
+    }
 }
