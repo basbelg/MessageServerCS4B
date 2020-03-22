6158015be80614239dac86d121268a329e54b2ec
basbelg <basbeez@live.com>
2020-03-21 20:02:53 -0700

    Packet - Added an alternate constructor

--- src/Messages/Packet.java
+++ src/Messages/Packet.java
@@ -2,7 +2,12 @@ package Messages;
 
 import java.io.Serializable;
 
-public class Packet implements Serializable{
+public class Packet implements Serializable {
     public String type;
     public Serializable value;
+
+    public Packet(String type, Serializable value) {
+        this.type = type;
+        this.value = value;
+    }
 }
