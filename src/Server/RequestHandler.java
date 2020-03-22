1b71099d9087ba38c719eef3a348bc3b0f6c997f
basbelg <basbeez@live.com>
2020-03-22 16:11:50 -0700

    RequestHandler - Added the BlockingQueue, List of Clients, HashMap of Clients subscribed to any particular channel, and the HashMap of a particular channel's history data fields

--- src/Server/RequestHandler.java
+++ src/Server/RequestHandler.java
@@ -1,6 +1,18 @@
 package Server;
 
+import Messages.Packet;
+
+import java.io.Serializable;
+import java.util.HashMap;
+import java.util.List;
+import java.util.concurrent.BlockingQueue;
+
 public class RequestHandler implements Runnable {
+    private BlockingQueue<Packet> requests;
+    private List<Client> clients;
+    private HashMap<String, List<Client>> subscribers;
+    private HashMap<String, List<Serializable>> history;
+
 
 
     @Override
